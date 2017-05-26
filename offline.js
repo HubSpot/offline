a17291864212aab9ae9325c7ce5a2e0f541e96bf
Ryan Berdeen <rberdeen@hubspot.com>
2017-05-26 15:38:57 -0400

    v0.7.19

--- offline.js
+++ offline.js
@@ -1,4 +1,4 @@
-/*! offline-js 0.7.18 */
+/*! offline-js 0.7.19 */
 (function() {
   var Offline, checkXHR, defaultOptions, extendNative, grab, handlers, init;
   extendNative = function(to, from) {
